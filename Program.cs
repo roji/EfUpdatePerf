876e9d13444782658ab3257cb465edb281f3fd1d
Shay Rojansky <roji@roji.org>
2024-01-16 22:55:55 +0100

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Diagnostics;
+
+await using var ctx = new BlogContext();
+await ctx.Database.EnsureDeletedAsync();
+await ctx.Database.EnsureCreatedAsync();
+
+for (var i = 0; i < 4; i++)
+{
+    ctx.Blogs.Add(new Blog
+    {
+        Name = "MyBlog" + i
+    });
+}
+await ctx.SaveChangesAsync();
+
+await foreach (var blog in ctx.Blogs)
+{
+    ctx.Remove(blog);
+}
+await ctx.SaveChangesAsync();
+
+await ctx.Blogs.ExecuteDeleteAsync();
+
+await ctx.Blogs
+    .ExecuteUpdateAsync(setters => setters
+        .SetProperty(b => b.Name, "bar")
+        .SetProperty(b => b.Rating, b => b.Rating + 1));
+
+public class BlogContext : DbContext
+{
+    public DbSet<Blog> Blogs { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        => optionsBuilder
+            .UseSqlServer("Server=localhost;Database=test;User=SA;Password=Abcd5678;Connect Timeout=60;ConnectRetryCount=0;Encrypt=false")
+            .LogTo(Console.WriteLine, new[]
+            {
+                RelationalEventId.TransactionCommitted,
+                RelationalEventId.TransactionStarted,
+                RelationalEventId.CommandExecuted
+            })
+            .EnableSensitiveDataLogging();
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Blog>().Property(b => b.Id).UseHiLo();
+    }
+}
+
+public class Blog
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public int Rating { get; set; }
+    
+    public List<Post> Posts { get; set; }
+}
+
+public class Post
+{
+    public int Id { get; set; }
+    public string Title { get; set; }
+    
+    public int BlogId { get; set; }
+}
